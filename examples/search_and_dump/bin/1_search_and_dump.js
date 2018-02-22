d03654e179a78867886b86426046d7dc39cc4709
Michael Kreil <github@michael-kreil.de>
2018-02-22 16:29:33 +0100

    scraping more hashtags

--- examples/search_and_dump/bin/1_search_and_dump.js
+++ examples/search_and_dump/bin/1_search_and_dump.js
@@ -4,65 +4,83 @@ const fs = require('fs');
 const xz = require('xz');
 const utils = require('../../../lib/utils.js');
 const colors = require('colors');
-const scraper = (require('../../../lib/scraper.js'))('hashtag');
+const scraper = (require('../../../lib/scraper.js'))('search_and_dump');
 const path = require('path');
-const parseQuery = (url => require('querystring').parse(require('url').parse(url).query));
 
-var dates = [];
-var today = Math.floor(Date.now()/86400000)+0.5;
-for (var i = -10; i < 0; i++) {
-	var d = Date.now()/86400000
-	dates.push((new Date((today+i)*86400000)).toISOString().substr(0,10));
-}
 
-var hashtags = [
-	{name: '34c3', query: '34c3'},
-	{name: 'iranprotests', query: 'تظاهرات_سراسری OR IranProtests'},
-	{name: 'netzdg', query: 'NetzDG'},
-	{name: 'olympics_2018', query: 'PyeongChang2018'},
-	{name: 'silvester', query: 'Silvester'},
-	{name: 'trump_mentions', query: 'to:realDonaldTrump OR to:POTUS'},
-	{name: 'trump_tweets', query: 'from:realDonaldTrump OR from:POTUS'},
+// List of search queries
+
+var queries = [
+	{name: '34c3',            query: {q: '34c3'}},
+	{name: 'iranprotests',    query: {q: 'تظاهرات_سراسری OR IranProtests'}},
+	{name: 'netzdg',          query: {q: 'NetzDG'}},
+	{name: 'olympics_2018',   query: {q: '#olympia2018 OR #pyeongchang2018 OR #olympics OR #olympia OR #doping'}},
+	{name: 'pyeongchang2018', query: {q: 'pyeongchang2018'}},
+	{name: 'trump_mentions',  query: {q: 'to:realDonaldTrump OR to:POTUS'}},
+	{name: 'trump_tweets',    query: {q: 'from:realDonaldTrump OR from:POTUS'}},
+	{name: 'emmanuelmacron',  query: {q: 'from:EmmanuelMacron OR to:EmmanuelMacron'}},
+	{name: 'elysee',          query: {q: 'from:Elysee OR to:Elysee'}},
+	{name: 'metoo',           query: {q: '#metoo'}},
+	{name: 'lufthansa',       query: {q: 'lufthansa OR ExploreTheNew'}},
+	{name: 'heimathorst',     query: {q: 'heimathorst OR heimatministerium'}},
+	{name: 'heimat',          query: {q: 'heimat'}},
+	{name: 'groko',           query: {q: 'groko'}},
+	{name: 'amadeuantonio',   query: {q: 'AmadeuAntonio OR from:AmadeuAntonio OR to:AmadeuAntonio'}},
+	{name: 'rechts_aufbruch', query: {q: 'aufbruchinsungewisse'}},
+	{name: 'rechts',          query: {q: 'Staatsfernsehen OR Merkelmussweg OR widerstand OR AfDwaehlen OR StopIslam OR Antifaverbot OR StopAsyl'}}, 
+	{name: '120db',           query: {q: 'Frauenmarsch OR 120db OR b1702 OR dd1702 OR ndh1702 OR niun1702 OR niun OR no120db'}}, 
+	{name: 'floridashooting', query: {q: 'emmagonzalez OR floridahighschool OR floridaschoolshooting OR floridashooter OR floridashooting OR floridastrong OR guncontrol OR guncontrolnow OR gunlawsnow OR gunreformnow OR gunsafety OR gunsense OR gunshooting OR highschoolshooter OR march4ourlives OR marchforourlives OR massshooting OR massshootings OR neveragain OR nrabloodmoney OR parklandschoolshooting OR parklandshooting OR righttobeararms OR schoolshooting'}},
 ]
 
-hashtags.forEach(obj => {
-	dates.forEach(date => {
-		runScraper(obj.name, obj.query, date);
-	})
-})
 
+// Search with each of these queries,
+// for each of the last 14 days
+
+var today = Math.floor(Date.now()/86400000-0.25)+0.5;
+for (var i = -14; i < 0; i++) {
+	var date = (new Date((today+i)*86400000)).toISOString().substr(0,10);
+	queries.forEach(obj => runScraper(obj.name, obj.query, date))
+}
+
+
+// Start scraper
 scraper.run();
+
+
 
 function runScraper(name, query, date) {
 	var filename = path.resolve(__dirname, '../data/'+name+'/'+name+'_'+date+'.jsonstream');
 
+	// Does the file already exists
 	if (fs.existsSync(filename) || fs.existsSync(filename+'.xz')) {
-		console.log(colors.yellow('Ignore "'+name+'" "'+date+'"'));
+		console.log(colors.grey('Ignore "'+date+'" - "'+name+'" '));
 		return;
 	}
 
+	// Make sure that the folder exists
 	utils.ensureDir(filename);
 
-
+	// Map of all found tweets
 	var tweets = new Map();
 
+	// new scraper sub task
 	var task = scraper.getSubTask()
 
-	scrape();
-
+	// when finished: sort and save the tweets
 	task.finished(() => {
 		tweets = Array.from(tweets.values());
 		if (tweets.length === 0) return;
-
 		tweets.sort((a,b) => a.id_str < b.id_str ? -1 : 1);
-
 		tweets = tweets.map(t => t.buffer);
-
 		saveBufferArray(filename, tweets);
 	})
 
+	// start recursive scraper
+	scrape();
+
 	function scrape(max_id) {
-		var attributes = {q:query, result_type:'recent', count:100, max_id:max_id};
+		var attributes = {result_type:'recent', count:100, max_id:max_id};
+		Object.keys(query).forEach(key => attributes[key] = query[key])
 
 		var minDate = new Date(date);
 		var maxDate = new Date(minDate.getTime()+86400000);
@@ -89,10 +107,10 @@ function runScraper(name, query, date) {
 
 				if (min_id) {
 					console.log([
-						'next_id: '+min_id,
-						'date: '+(result.statuses[0]||{}).created_at,
-						'count: ' + tweets.size
-					].join('\t'));
+						(result.statuses[0]||{}).created_at.replace(/ \+.*/,''),
+						name,
+						'(' + tweets.size + ')'
+					].join('   '));
 
 					scrape(min_id);
 				}
@@ -101,6 +119,9 @@ function runScraper(name, query, date) {
 	}
 }
 
+
+// Gets a filename and an array with buffers
+// and saves these buffers as a xz compressed file
 function saveBufferArray(filename, bufs) {
 	filename += '.xz';
 
@@ -122,7 +143,7 @@ function saveBufferArray(filename, bufs) {
 		bufs = bufs.slice(lines);
 
 		if (compression.write(chunk)) {
-			setTimeout(write, 1);
+			setTimeout(write, 0);
 		} else {
 			compression.once('drain', () => {
 				write();
