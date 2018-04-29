623a72488f458e79493647182df5e6f2381dbdf9
Michael Kreil <github@michael-kreil.de>
2018-04-29 10:47:56 +0200

    improve fake news simulator

--- bin/fake_news_simulator/web/main.js
+++ bin/fake_news_simulator/web/main.js
@@ -6,11 +6,12 @@ $(function () {
 	var doRedraw = false;
 	var nodeCount = 3000;
 	var mouseX, mouseY, mousedown = false, selectedNode;
+	var slow = true, startTime = 1e50;
 
 	canvas.mousedown(e => {mousedown = true; click(e)});
 	canvas.mouseup(() => mousedown = false);
 	canvas.mouseleave(() => mousedown = false);
-	canvas.mousemove(e => {if (mousedown) click(e)});
+	//canvas.mousemove(e => {if (mousedown) click(e)});
 	
 	resize();
 	$(window).resize(resize);
@@ -28,18 +29,19 @@ $(function () {
 	var data;
 
 	generateData();
+	//simulateFakeNews();
 	doRedraw = 1;
 	init();
 
 	function init() {
 		simulation = d3.forceSimulation(data.nodes);
 		simulation.alphaMin(0.1);
-		simulation.alphaDecay(1 - Math.pow(simulation.alphaMin(), 1 / 80)),
+		simulation.alphaDecay(1 - Math.pow(simulation.alphaMin(), 1 / 100)),
 
 		simulation.force('charge', forceRepulsion());
 		function forceRepulsion() {
 			var nodes,
-			    strength = -50,
+			    strength = -100,
 			    theta2 = 1;
 
 			function force(alpha) {
@@ -162,7 +164,7 @@ $(function () {
 
 		simulation.on('tick', () => doRedraw += 0.1);
 		simulation.on('end', () => {
-			console.log('Finished')
+			console.log('Finished');
 			doRedraw = 2;
 		});
 
@@ -170,7 +172,7 @@ $(function () {
 	}
 
 	function redraw() {
-		if (mousedown && selectedNode) /*for (var i = 0; i < 100; i++)*/ markFakeNews(selectedNode);
+		//if (mousedown && selectedNode) markFakeNews(selectedNode, slow);
 
 		if (doRedraw < 1) return;
 		if (!simulation) return;
@@ -197,26 +199,57 @@ $(function () {
 			n.pr = retina*Math.sqrt(n.weight)*2;
 		})
 
+		var playTime = 10*(Date.now()-startTime)/1000;
+
 		if (doRedraw >= 2) {
 			ctx.lineWidth = 0.5*retina;
 			data.edges.sort((a,b) => a.prio - b.prio);
 			data.edges.forEach(e => {
-				ctx.strokeStyle = e.color;
+				if (playTime < 0) {
+					ctx.strokeStyle = '#00a';
+				} else if (playTime < e.endTime) {
+					ctx.strokeStyle = '#bbf';
+				} else if (playTime > e.endTime) {
+					ctx.strokeStyle = '#c00';
+				}
 				ctx.beginPath();
 				ctx.moveTo(e.source.px, e.source.py);
 				ctx.lineTo(e.target.px, e.target.py);
 				ctx.stroke();
+
+				if ((playTime >= e.startTime) && (playTime < e.endTime) ) {
+					var a = (playTime-e.startTime)/(e.endTime-e.startTime);
+					var s = e.forward ? e.source : e.target;
+					var t = e.forward ? e.target : e.source;
+					ctx.strokeStyle = '#c00';
+					ctx.beginPath();
+					ctx.moveTo(s.px, s.py);
+					ctx.lineTo(
+						(t.px-s.px)*a+s.px,
+						(t.py-s.py)*a+s.py,
+					);
+					ctx.stroke();
+				}
 			})
 		}
 
 		data.nodes.forEach(n => {
-			ctx.fillStyle = n.color;
+			if (playTime < 0) {
+				ctx.fillStyle = '#00a';
+			} else if (playTime < n.gotTime) {
+				ctx.fillStyle = '#bbf';
+			} else if (playTime < n.sendTime) {
+				ctx.fillStyle = '#c66';
+			} else {
+				ctx.fillStyle = '#c00';
+			}
 			ctx.beginPath();
 			ctx.arc(n.px, n.py, n.pr, 0, 2*Math.PI);
 			ctx.fill();
 		})
 
-		doRedraw = 0;
+		if (startTime > 1e49) return doRedraw = 0;
+		if (playTime > 100) return doRedraw = 0;
 	}
 
 	function click(e) {
@@ -232,56 +265,147 @@ $(function () {
 			}
 		})
 		selectedNode = minNode;
+
+		markFakeNews(selectedNode);
+		startTime = Date.now()+0;
 	}
 
 	function markFakeNews(startNode) {
 		if (!startNode) return;
 
-		doRedraw = 2;
+		var todos = [];
 
+		data.edges.forEach(e => {
+			e.prio = 1e50;
+			e.startTime = 1e50;
+			e.endTime = 1e50;
+			e.transmitted = false;
+		});
+		data.nodes.forEach(n => {
+			n.gotTime = 1e50;
+			n.sendTime = 1e50;
+			n.received = false;
+			n.send = false;
+		})
+
+		nodeReceived(startNode, -1);
+		nodeSend(startNode, Math.random());
+
+		function nodeReceived(node, time) {
+			if (node.received) return;
+			node.received = true;
+
+			node.gotTime = time;
+			if (node.prop > Math.random()) {
+				todos.push([nodeSend, node, time+Math.random()*1]);
+				return true;
+			}
+		}
+
+		function nodeSend(node, time) {
+			var changes = false
+			if (node.send) return;
+			node.send = true;
+
+			node.sendTime = time;
+			node.neighbours.forEach(entry => {
+				var node2 = entry[0];
+				var edge = entry[1];
+
+				if (!edge.transmitted) {
+					edge.prio = time;
+					edge.startTime = time;
+					edge.endTime = time + edge.length/100;
+					edge.forward = (edge.source === node);
+					edge.transmitted = true;
+
+					if (!node2.received) {
+						todos.push([nodeReceived, node2, edge.endTime]);
+						changes = true;
+					}
+				}
+			})
+
+			return changes;
+		}
+
+
+		todos.sort((a,b) => a[2]-b[2]);
+		while (todos.length > 0) {
+			var todo = todos.shift();
+			var n = todos.length;
+			var result = todo[0].call(this, todo[1], todo[2]);
+			if (result) todos.sort((a,b) => a[2]-b[2]);
+		}
+
+		//console.log(end);
+
+		//console.table(data.nodes);
+		//console.table(data.edges);
+
+		
+
+		doRedraw = 2;
+/*
 		data.edges.forEach(e => {
 			e.color = '#bbf';
 			e.prio = 2;
+			e.forward = true;
+			e.checked = false;
+			e.
 		});
 		data.nodes.forEach(n => {
 			n.checked = false;
 			n.sendFake = false;
-			n.receivedFake = false;
+			n.gotFake = false;
 			n.color = '#bbf';
+			n.startTime = 1e10;
 		})
 
 		startNode.color = '#c00';
 		startNode.checked = true;
 		startNode.sendFake = true;
-		startNode.receivedFake = true;
+		startNode.gotFake = true;
+		startNode.startTime = 0;
 		var checkSender = new Set([startNode]), checkReceiver;
 
-		while (checkSender.size > 0) {
+		do {
 			didSomething = false;
 			checkReceiver = new Set();
 
 			Array.from(checkSender.values()).forEach(n => {
 				n.neighbours.forEach(entry => {
-					if (!entry[0].checked) checkReceiver.add(entry[0]);
-					entry[1].color = '#c00';
-					entry[1].prio = 0;
+					var edge = entry[1];
+					if (!edge.checked) {
+						edge.color = '#c00';
+						edge.prio = 0;
+						edge.startTime = n.startTime;
+						edge.forward = edge.source === n;
+						edge.checked = true;
+					}
+
+					if (!entry[0].checked) {
+						checkReceiver.add(entry[0]);
+						entry[0].startTime = n.startTime+1;
+					}
 				})
 			})
 
 			checkSender = new Set();
 			Array.from(checkReceiver.values()).forEach(n => {
-				
-				n.receivedFake = true;
+				n.gotFake = true;
 				if (n.prop > Math.random()) {
 					n.color = '#c00';
 					n.sendFake = true;
 					checkSender.add(n);
 				} else {
-					n.color = '#e90';	
-				}
+					n.color = '#e90';				}
 				n.checked = true;
 			})
-		}
+		} while (!slow && (checkSender.size > 0));
+
+		if (slow && (checkSender.size > 0)) setTimeout(() => markFakeNews(false, true), 300);
+		*/
 	}
 
 	function generateData() {
@@ -290,36 +414,39 @@ $(function () {
 		var nodes = [];
 		for (var i = 0; i < nodeCount; i++) {
 			var node = getRandomNode(0,0,maxR/2);
+			node.group = 0;
 			node.prop = 0.1;
 			nodes.push(node);
 		}
 
-		/*
+		
 		for (var i = 0; i < nodeCount*0.1; i++) {
-			var node = getRandomNode(-maxR*0.7,0,0.2*maxR/2);
+			var node = getRandomNode(maxR*0.6,0,0.2*maxR/2);
+			node.group = 1;
 			node.prop = 0.3;
 			nodes.push(node);
 		}
-		*/
+		
+		
 
 		var edges = [];
 		var nodesLeft = nodes;
 		while (nodesLeft.length > 0) {
-			var node = nodesLeft.pop();
+			var node = nodesLeft[Math.floor(nodesLeft.length*Math.random())];
 			
 			nodesLeft.forEach(n => {
 				n.d = Math.sqrt(sqr(node.x-n.x) + sqr(node.y-n.y)) + Math.random()*maxR*0.2;
 			})
 			nodesLeft.sort((a,b) => a.d - b.d);
 
-			for (var i = 0; i < node.count; i++) {
+			for (var i = 0; i < Math.min(nodesLeft.length, node.count); i++) {
 				var rNode = nodesLeft[i];
-				if (!rNode) continue;
 				var edge = {
 					source:node,
 					target:rNode,
 					weight:1,//Math.sqrt(node.weight*rNode.weight)
-					color:'#00f',
+					//color:'#00f',
+					length:rNode.d,
 				};
 				edges.push(edge);
 				node.count--;
@@ -346,7 +473,7 @@ $(function () {
 				y: y0 + r*Math.sin(alpha),
 				count: count,
 				weight: count/10,
-				color: '#00a',
+				//color: '#00a',
 				neighbours: [],
 				prop: 0.1,
 			}
@@ -355,6 +482,26 @@ $(function () {
 		function gauss() {
 			return (Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random() - 3) / 3;
 		}
+	}
+
+	function simulateFakeNews() {
+		var histo = [];
+
+		for (var i = 0; i < 10000; i++) {
+			var sumGot = 0, sumSend = 0, sumNone = 0;
+			var node = data.nodes[Math.floor(data.nodes.length*Math.random())];
+			markFakeNews(node);
+			data.nodes.forEach(n => {
+				if (!n.gotFake ) return sumNone++;
+				if (!n.sendFake) return sumGot++;
+				sumSend++;
+			})
+			if (histo[sumNone]) histo[sumNone].none++; else histo[sumNone] = {none:1, got:0, send:0};
+			if (histo[sumGot ]) histo[sumGot ].got++;  else histo[sumGot ] = {none:0, got:1, send:0};
+			if (histo[sumSend]) histo[sumSend].send++; else histo[sumSend] = {none:0, got:0, send:1};
+		}
+
+		//console.table(histo);
 	}
 
 })
