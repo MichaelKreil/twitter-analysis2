7e6c0ca51d25bbdd4c940fe0d4c1b7016142d600
Michael Kreil <github@michael-kreil.de>
2018-06-04 14:55:53 +0200

    add github_microsoft

--- bin/search_and_dump/1_search_and_dump.js
+++ bin/search_and_dump/1_search_and_dump.js
@@ -86,6 +86,7 @@ var queries = [
 	{name: 'fakenewssource1',               query: {q:'berliner-express.com OR truth24.net'}},
 	{name: 'floridashooting',               query: {q:'emmagonzalez OR floridahighschool OR floridaschoolshooting OR floridashooter OR floridashooting OR floridastrong OR guncontrol OR guncontrolnow OR gunlawsnow OR gunreformnow OR gunsafety OR gunsense OR gunshooting OR highschoolshooter OR march4ourlives OR marchforourlives OR massshooting OR massshootings OR neveragain OR nrabloodmoney OR parklandschoolshooting OR parklandshooting OR righttobeararms OR schoolshooting'}},
 	{name: 'floridashooting2',              query: {q:'neveragain OR gunreformnow OR guncontrolnow OR guncontrol OR marchforourlives OR parkland OR parklandschoolshooting OR floridaschoolshooting OR parklandshooting OR #nra OR floridashooting OR nrabloodmoney OR banassaultweapons OR gunsense OR emmagonzalez OR schoolshooting OR parklandstudents OR parklandstudentsspeak OR gunviolence OR floridashooter OR wecallbs OR studentsstandup OR parklandstrong'}},
+	{name: 'github_microsoft',              query: {q:'github microsoft'}},
 	{name: 'groko',                         query: {q:'groko'}},
 	{name: 'heimat',                        query: {q:'heimat'}},
 	{name: 'heimathorst',                   query: {q:'heimathorst OR heimatministerium'}},
