b040c2fc6f597b427c5f76c68c5dd571108ae2ea
Michael Kreil <github@michael-kreil.de>
2018-06-12 11:25:39 +0200

    run it parallel

--- bin/world2/1_fetch_data/2_fetch_friends.js
+++ bin/world2/1_fetch_data/2_fetch_friends.js
@@ -1,28 +1,40 @@
 "use strict"
 
 const config = require('../config.js');
+const fetchFriends = require('../lib/twitter.js').fetchFriends;
+const fs = require('fs');
 const miss = require('../lib/mississippi.js');
 const resolve = require('path').resolve;
-const fetchFriends = require('../lib/twitter.js').fetchFriends;
 
-var fileId = resolve(__dirname, '../../../data/world/1_ids/ids_selected_'+config.activityMinimumName+'_'+config.step+'.tsv.gz');
-var fileDb = resolve(__dirname, '../../../data/world/dbs/friends.tsv.gz');
+var dir = resolve(__dirname, '../../../data/world/');
+var fileIdIn    = resolve(dir, '1_ids/ids_selected_'+config.activityMinimumName+'_'+config.step+'.tsv.gz');
+var fileDbIn  = resolve(dir, 'dbs/friends_'+(config.step  )+'.tsv.gz');
+var fileDbOut = resolve(dir, 'dbs/friends_'+(config.step+1)+'.tsv.gz');
 
-var active = 0;
+if (!fs.existsSync(fileIdIn)) throw Error('Missing '+fileIdIn);
+if (!fs.existsSync(fileDbIn)) throw Error('Missing '+fileDbIn);
 
 miss.pipe(
-	miss.merge(
-		miss.readGzipLines(fileId),
-		miss.readGzipLines(fileDb),
-		(idId, lineId, idDb, lineDb, cb) => {
-			console.log(idId+'\t'+idDb);
-			if (idDb !== null) return cb(null, lineDb);
-			
-			fetchFriends(idId, friends => {
+	miss.mergeId(
+		miss.readGzipLines(fileIdIn),
+		miss.readGzipLines(fileDbIn)
+	),
+	miss.parallel(
+		64,
+		(data, cb) => { // data = [idId, lineId, idDb, lineDb]
+			if (data[2] !== null) return cb(null, data[3]);
+
+			//console.log(data);
+			var id = data[0];
+			if (!id) throw Error();
+
+			if (id.slice(0,2) >= '14') return cb();
+
+			fetchFriends(id, friends => {
 				friends.sort();
-				cb(null, idId+'\t'+friends.join(','));
+				cb(null, id+'\t'+friends.join(','));
 			})
 		}
 	),
-	miss.writeGzipLines(fileDb+'.tmp.gz'),
+	miss.writeGzipLines(fileDbOut),
 )
