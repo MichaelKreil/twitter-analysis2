cf86a578510db20e8a2bb583a5a33be674fa48f0
Michael Kreil <github@michael-kreil.de>
2019-05-23 15:30:17 +0200

    improve dumping process and add logging

--- bin/dump_cache.js
+++ bin/dump_cache.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var name = 'botometer2';
+var name = 'botometer_2019-05-08_2';
 
 const fs = require('fs');
 const miss = require('mississippi2');
@@ -10,14 +10,42 @@ const lzma = require('lzma-native');
 var path = require('path').resolve(__dirname, '../cache/', name);
 var db = Levelup(path, {keyEncoding:'utf8', valueEncoding:'utf8'});
 
-db.createReadStream()
-	.pipe(miss.through.obj((chunk, enc, cb) => cb(null, JSON.stringify(chunk)+'\n')))
-	.pipe(lzma.createCompressor(
-		{
+var count = 0, index = 0, startTime;
+countEntries(() => dumpEntries())
+
+function countEntries(cb) {
+	console.log('Counting ...')
+	miss.pipe(
+		db.createKeyStream(),
+		miss.to.obj((chunk, enc, cb) => {
+			count++;
+			if (count % 10000 === 0) console.log('   '+count);
+			cb();
+		}),
+		() => { if (cb) cb() }
+	)
+}
+
+function dumpEntries(cb) {
+	console.log('Dumping '+count+' entries ...');
+	startTime = Date.now();
+	miss.pipe(
+		db.createReadStream(),
+		miss.through.obj((chunk, enc, cb) => {
+			index++;
+			if (index % 1000 === 0) console.log([
+				'   '+(100*index/count).toFixed(1)+'%   ',
+				(new Date((Date.now()-startTime)*(count-index)/index)).toLocaleTimeString()
+			].join(''));
+			cb(null, JSON.stringify(chunk)+'\n')
+		}),
+		lzma.createCompressor({
 			check: lzma.CHECK_NONE,
 			preset: 9,
 			synchronous: false,
 			threads: 1,
-		}
-	))
-	.pipe(fs.createWriteStream('dbdump_'+name+'.xz'));
+		}),
+		fs.createWriteStream('dbdump_'+name+'.xz'),
+		() => { if (cb) cb() }
+	)
+}
