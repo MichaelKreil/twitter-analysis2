e561962fbb7f83434208876a2360a3ae7dd99a2a
Michael Kreil <github@michael-kreil.de>
2019-08-02 16:49:43 +0200

    update script

--- search_and_dump.sh
+++ search_and_dump.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 cd ${0%/*};
 
 message() {
-	curl -X POST --data-urlencode "payload={\"channel\":\"#monitoring\", \"username\":\"kreilbot\", \"link_names\":\"1\", \"text\":\"Error for <@U8LAL8Z5E> - $1\", \"icon_emoji\":\":rotating_light:\"}" https://hooks.slack.com/services/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;
+	# curl -X POST --data-urlencode "payload={\"channel\":\"#monitoring\", \"username\":\"kreilbot\", \"link_names\":\"1\", \"text\":\"Error for <@U8LAL8Z5E> - $1\", \"icon_emoji\":\":rotating_light:\"}" https://hooks.slack.com/services/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;
 	exit 1
 }
 
 git pull
 node ./bin/search_and_dump/1_search_and_dump.js || message 'search_and_dump.js failed'
-rm -r ./cache/search_and_dump
-node /home/igg/apps/sync/bin/dropbox.js || message 'dropbox.js failed'
+# rm -r ./cache/search_and_dump
+# node /home/igg/apps/sync/bin/dropbox.js || message 'dropbox.js failed'
