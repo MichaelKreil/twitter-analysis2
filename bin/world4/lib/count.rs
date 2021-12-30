b268e0d45d03f8e5003a2786dfcd15f9376d929d
Michael Kreil <github@michael-kreil.de>
2021-12-30 14:05:19 +0100

    add count

---
+++ bin/world4/lib/count.rs
@@ -0,0 +1,61 @@
+// rustc -C opt-level=3 ./count.rs
+// xz -dkc 4_friends-2021-12-28-22-53-25.tsv.xz | head -n 3 | grep -Eo '\[.*\]' | ~/projects/twitter-analysis2/bin/world4/lib/count
+
+use std::collections::BTreeMap;
+use std::io::{prelude::*};
+use std::env;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	let mut min_count:u32 = 0;
+	if args.len() > 1 {
+		min_count = args[1].parse::<u32>().unwrap();
+	}
+
+	let mut id_count: BTreeMap<u64,u32> = BTreeMap::new();
+	let stdin = std::io::stdin();
+	let lines = stdin.lock().lines();
+
+	for line in lines {
+		let text = line.unwrap();
+		//println!("{}", text);
+
+		let mut in_number: bool = false;
+		let mut start_index = 1;
+		let mut number:&str;
+		let mut id:u64;
+
+		for (i, c) in text.chars().enumerate() {
+			if c.is_numeric() {
+				if !in_number {
+					in_number = true;
+					start_index = i;
+				}
+			} else {
+				if in_number {
+					in_number = false;
+					number = &text[start_index..i];
+					id = number.parse::<u64>().unwrap();
+					*id_count.entry(id).or_insert(0) += 1;
+				}
+			}
+
+			//println!("{} {} {}", i, c, c.is_numeric());
+		}
+
+		//let id = line.unwrap().parse::<u64>().unwrap();
+		//ids.insert(id);
+	}
+
+	for (id, count) in id_count.iter() {
+		if count < &min_count {
+			continue
+		}
+		//println!("{}: {}", id, count);
+		println!("{}", id);
+	}
+}
+
+
+
+//let v = bytes.iter().cloned().filter(|c| b"0123456789".contains(c) ).map(|c| c - b'0').collect::<Vec<u8>>();
