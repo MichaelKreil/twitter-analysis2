0e3c0b1700e252fb1385afc92b492e2900d33a86
Michael Kreil <github@michael-kreil.de>
2022-01-06 21:03:50 +0100

    Update 1_scrape_status.js

--- bin/world4/1_scrape_status.js
+++ bin/world4/1_scrape_status.js
@@ -17,24 +17,17 @@ start()
 function start() {
 	miss.pipeline(
 		Readable.from(getBlocks()),
-		transform(16, (block, callback) => {
+		transform(16, (ids, callback) => {
 			let now = Date.now();
-			let ids = block.ids.map(i => i[0]);
 
 			scraper.fetch(
 				'users/lookup',
 				{ user_id:ids.join(','), include_entities:false, tweet_mode:'extended' },
 				result => {
-					let lookup = new Map(block.ids.map(e => [e[0],e]));
-					result.forEach(u => {
-						u.now = now;
-						let line = lookup.get(u.id_str);
-						if (!line) return;
-						line[0] = u.id_str+'\t'+JSON.stringify(u);
-					})
-					block = block.lines.map(l => l[0]).join('\n')+'\n';
-					block = Buffer.from(block);
-					callback(null, block);
+					result.sort((a,b) => (a.id_str.length - b.id_str.length) || (a.id_str < b.id_str ? -1 : 1);
+					result = result.map(e => JSON.stringify(e)+'\n').join('');
+					result = Buffer.from(result);
+					callback(null, result);
 				}
 			)
 		}),
@@ -44,34 +37,19 @@ function start() {
 
 async function* getBlocks() {
 	const maxIdCount = 100;
-	const maxLineCount = 10000;
 
-	let block = newBlock();
-	let files = ['1_id-2021-12-28-13-04-00.txt.xz'].map(f => resolve(dataFolder, f));
+	let block = [];
 
-	for await (let entry of readLinesMulti(files)) {
-		let key = entry.key;
-		if (!key) throw Error();
+	for await (let id of readXzLines(resolve(dataFolder, '1_ids-2022-01-06-20-53-48.tsv.xz'))) {
+		if (!id) throw Error('id is missing');
+		block.ids.push(line);
 
-		if (!entry.lines[0]) throw Error('id in id file is missing');
-
-		if (entry.lines[1]) {
-			block.lines.push([lines[1]]);
-		} else {
-			let line = [key]
-			block.lines.push(line);
-			block.ids.push(line);
-		}
-
-		if ((block.ids.length >= maxIdCount) || (block.lines.length >= maxLineCount)) {
+		if (block.length >= maxIdCount) {
 			yield block;
-			block = newBlock();
+			block = [];
 		}
 	}
 	console.log('Finished');
 	if (block.lines.length > 0) yield block;
 
-	function newBlock() {
-		return {ids:[],lines:[]};
-	}
 }
