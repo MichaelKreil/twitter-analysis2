1973b08e7b23e2781e8e1b2e726b6e0aec5fd9da
Michael Kreil <github@michael-kreil.de>
2022-01-06 14:31:53 +0100

    scan friends

---
+++ bin/world4/4_scan_friends.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd ${0%/*};
+
+rustc -C opt-level=3 -o lib/count lib/count.rs
+printf -v date '%(%Y-%m-%d-%H-%M-%S)T' -1
+pv ~/data/twitter/world4/4_friends-2021-12-28-22-53-25.tsv.xz | xz -d | lib/count 300 | xz -z9 > ~/data/twitter/world4/5_friend_ids-${date}.tsv.xz
