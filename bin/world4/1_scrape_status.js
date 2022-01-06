19b62f6e32d988c675102c1d89f93e5b9c7e542f
Michael Kreil <github@michael-kreil.de>
2022-01-06 21:12:03 +0100

    fixes

--- bin/world4/1_scrape_status.js
+++ bin/world4/1_scrape_status.js
@@ -7,8 +7,8 @@ const { resolve } = require('path');
 const miss = require('mississippi2');
 const transform = require('parallel-transform');
 
-const scraper = require('../../lib/scraper.js')('test');
-const { readLinesMulti, xzWriter, getTimeSlug } = require('./lib/helper.js');
+const scraper = require('../../lib/scraper.js')('temp');
+const { readXzLines, xzWriter, getTimeSlug } = require('./lib/helper.js');
 
 const dataFolder = '/root/data/twitter/world4'
 
@@ -24,7 +24,7 @@ function start() {
 				'users/lookup',
 				{ user_id:ids.join(','), include_entities:false, tweet_mode:'extended' },
 				result => {
-					result.sort((a,b) => (a.id_str.length - b.id_str.length) || (a.id_str < b.id_str ? -1 : 1);
+					result.sort((a,b) => (a.id_str.length - b.id_str.length) || (a.id_str < b.id_str ? -1 : 1));
 					result = result.map(e => JSON.stringify(e)+'\n').join('');
 					result = Buffer.from(result);
 					callback(null, result);
@@ -40,9 +40,9 @@ async function* getBlocks() {
 
 	let block = [];
 
-	for await (let id of readXzLines(resolve(dataFolder, '1_ids-2022-01-06-20-53-48.tsv.xz'))) {
-		if (!id) throw Error('id is missing');
-		block.ids.push(line);
+	for await (let line of readXzLines(resolve(dataFolder, '1_ids-2022-01-06-20-53-48.tsv.xz'), true)) {
+		if (!line) throw Error('id is missing');
+		block.push(line);
 
 		if (block.length >= maxIdCount) {
 			yield block;
@@ -50,6 +50,6 @@ async function* getBlocks() {
 		}
 	}
 	console.log('Finished');
-	if (block.lines.length > 0) yield block;
+	if (block.length > 0) yield block;
 
 }
