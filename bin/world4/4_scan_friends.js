3b9ae61046d70a2481bc6e6c10310ec6b8a9a21b
Michael Kreil <github@michael-kreil.de>
2022-01-07 13:00:43 +0100

    implement as node

---
+++ bin/world4/4_scan_friends.js
@@ -0,0 +1,23 @@
+"use strict"
+
+const fs = require('fs');
+const { Readable } = require('stream');
+
+const miss = require('mississippi2');
+
+const { findDataFile, getDataFile, getTempFile, getXZ, xzWriter, count, jq } = require('./lib/helper.js');
+
+start()
+
+function start() {
+	let tempFilename = getTempFile();
+	let dataFilename = getDataFile('5_friend_ids');
+	
+	miss.pipe(
+		getXZ(findDataFile('4_friends'), true),
+		jq('.ids? | join(",")'),
+		count(300),
+		xzWriter(tempFilename),
+		() => fs.renameSync(tempFilename, dataFilename)
+	)
+}
