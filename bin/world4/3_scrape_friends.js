65af2ad7da64a725158439b7bafc96e8d39eb31a
Michael Kreil <github@michael-kreil.de>
2022-01-08 16:32:23 +0100

    debug stuff

--- bin/world4/3_scrape_friends.js
+++ bin/world4/3_scrape_friends.js
@@ -18,29 +18,35 @@ function start() {
 	let index = 0;
 	let active = 0;
 	miss.pipe(
-		Readable.from(readLinesMulti([findDataFile('3_ids'), findDataFile('4_friends')])),
+		Readable.from(readLinesMulti([
+			findDataFile('3_ids'),
+			findDataFile('4_friends'),
+		])),
 		transformParallel(16, (entry, callback) => {
 			if (!entry.lines[0]) return callback();
 			if ( entry.lines[1]) return callback(null, entry.lines[1]+'\n');
+			//console.log(entry);
 
 			let id = entry.key;
 
-			active++;
-			let start = Date.now();
+			//active++;
+			//let start = Date.now();
+			//console.log(active, id);
 
 			scraper.fetch(
 				'friends/ids',
 				{ user_id:id, stringify_ids:true, count:5000 },
 				result => {
-					active--;
-					console.log(active, Date.now()-start);
+					//active--;
+					//console.log(active, Date.now()-start);
 					
 					result = { ids:result.ids, now:Date.now() };
 					callback(null, id+'\t'+JSON.stringify(result)+'\n');
 				}
 			)
 		}),
-		xzWriter(tempFilename, 1),
+		//miss.to((d,e,c) => c()),
+		xzWriter(tempFilename, 9),
 		() => fs.renameSync(tempFilename, dataFilename)
 	)
 }
