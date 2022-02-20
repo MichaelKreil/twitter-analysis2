ed7ec290a29ffe05fe3c5ba18fd0d96e213a85d5
Michael Kreil <github@michael-kreil.de>
2022-02-20 02:14:50 +0100

    Update count_in_array.rs

--- bin/world4/lib/count_in_array.rs
+++ bin/world4/lib/count_in_array.rs
@@ -1,100 +1,106 @@
+// test: rustc count_in_array.rs
 
-use std::io::prelude::*;
+#![allow(unused_must_use)]
+
+//use std::io::BufReader;
+//use std::mem::{size_of, transmute};
+use std::collections::BTreeMap;
 use std::env;
-use std::mem::{size_of, transmute};
 use std::fs::File;
+use std::io::BufWriter;
+use std::io::prelude::*;
+//use std::rc::Rc;
 
 const BUFFER_MAX_COUNT:usize = 128*1024;
 
-#[derive(Debug)]
-#[repr(C)]
-struct Entry {
-	id: u64,
-	count: u32,
-}
+//#[derive(Debug)]
+//#[repr(C)]
+//struct Entry {
+//	id: u64,
+//	count: u32,
+//}
 
+	/*
 impl Entry {
-	fn readFrom(mut file: File) -> Self {
-		//let mut file = File::open(path)?;
-
+	fn readFrom(mut file: &BufReader) -> Self {
 		let entry: Entry = {
 			let mut h = [0u8; size_of::<Entry>()];
-
 			file.read_exact(&mut h[..]).unwrap();
-
 			unsafe { transmute(h) }
 		};
-
 		return entry;
 	}
 
-	fn writeTo(self, mut file: &File) {
-		//let mut file = File::create(path)?;
-
+	fn writeTo(self, mut bufWriter: BufWriter<File>) {
 		let bytes: [u8; size_of::<Entry>()] = unsafe { transmute(self) };
-
-		file.write_all(&bytes).unwrap();
+		bufWriter.write(&bytes).unwrap();
 	}
 }
+	*/
 
 struct Database {
-	buffer_in: Vec<u64>,
-	filenames: Vec<String>,
+	map: BTreeMap<u64,u32>,
+	filename: String,
+	is_ready_to_write: bool,
 }
 
 impl Database {
-	fn new() -> Self {
+	fn new(index: usize) -> Self {
 		let mut _self = Database {
-			buffer_in: Vec::new(),
-			filenames: Vec::new(),
+			map: BTreeMap::new(),
+			filename: format!("count_in_array_{}.tmp", index),
+			is_ready_to_write: true,
 		};
-		_self.buffer_in.reserve(BUFFER_MAX_COUNT);
-		_self.reset_buffer();
 		return _self;
 	}
 
 	fn add(&mut self, id: &u64) {
-		self.buffer_in.push(*id);
-
-		if self.buffer_in.len() >= BUFFER_MAX_COUNT {
-			self.flush_buffer();
-			self.reset_buffer();
-		}
+		assert!(self.is_ready_to_write, "not ready to add data");
+		*self.map.entry(*id).or_insert(0) += 1;
 	}
 
-	fn reset_buffer(&mut self) {
-		eprintln!("reset_buffer");
-		self.buffer_in.clear();
+	fn flush(&mut self) {
+		let file = File::create(&self.filename).unwrap();
+		let mut buffered_file = BufWriter::with_capacity(65536, file);
+		for (id, count) in self.map.iter() {
+			buffered_file.write(&id.to_le_bytes());
+			buffered_file.write(&count.to_le_bytes());
+		}
+		buffered_file.flush();
+		self.is_ready_to_write = false;
+
+		drop(buffered_file);
+		self.map.clear();
 	}
 
-	fn flush_buffer(&mut self) {
-		eprintln!("flush_buffer");
+	fn is_full(&self) -> bool {
+		assert!(self.is_ready_to_write, "not ready to read fullness");
+		return self.map.len() >= BUFFER_MAX_COUNT;
+	}
+}
 
-		eprintln!("   sort");
-		self.buffer_in.sort_unstable();
-		eprintln!("   0: {}", self.buffer_in[0]);
-		eprintln!("   {}: {}", self.buffer_in.len(), self.buffer_in[self.buffer_in.len() - 1]);
+struct DatabaseWrapper {
+	list: Vec<Database>,
+}
 
-		eprintln!("   count");
-		let mut entry:Entry = Entry { id:0, count:0 };
-		let mut file = File::create("test.tmp").unwrap();
+impl DatabaseWrapper {
+	fn new() -> Self {
+		let mut _self = DatabaseWrapper {
+			list: Vec::new(),
+		};
+		_self.list.push(Database::new(0));
+		return _self;
+	}
 
-		for next_id in self.buffer_in.iter_mut() {
-			if entry.id == *next_id {
-				entry.count += 1;
-			} else {
-				if entry.id != 0 {
-					entry.writeTo(&file);
-				}
-				entry = Entry { id: *next_id, count: 1 };
-			}
+	fn add(&mut self, id: &u64) {
+		let database:&mut Database = self.list.last_mut().unwrap();
+		if database.is_full() {
+			database.flush();
+			&self.list.push(Database::new(self.list.len()));
+			self.list.last_mut().unwrap().add(&id);
+		} else {
+			database.add(&id);
 		}
-		entry.writeTo(&file);
-
-
-		//buffer.iter_mut().for_each(|m| *m = 0)
-		//self.reset_buffer();
-		eprintln!("finished");
 	}
 }
 
@@ -106,7 +112,7 @@ fn main() {
 	}
 	eprintln!("min_count: {}", min_count);
 
-	let mut database = Database::new();
+	let mut databases = DatabaseWrapper::new();
 
 	let stdin = std::io::stdin();
 	let lines = stdin.lock().lines();
@@ -114,6 +120,7 @@ fn main() {
 	for line in lines {
 		let text = line.unwrap();
 
+		// scan line
 		let mut in_number: bool = false;
 		let mut in_brackets: bool = false;
 		let mut id:u64 = 0;
@@ -130,7 +137,7 @@ fn main() {
 				} else {
 					if in_number {
 						in_number = false;
-						database.add(&id);
+						databases.add(&id);
 					}
 					if code == 93 {
 						in_brackets = false;
@@ -144,7 +151,8 @@ fn main() {
 		}
 	}
 
-	database.flush_buffer();
+	//println!("{}", databases.len())
+	//database.flush_buffer();
 /*
 	for (id, count) in id_count.iter() {
 		if count < &min_count {
