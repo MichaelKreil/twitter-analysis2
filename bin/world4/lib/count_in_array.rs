d82b0445d012a2338055a8ad1e759fbb20920e2f
Michael Kreil <github@michael-kreil.de>
2022-02-21 14:56:49 +0100

    Update count_in_array.rs

--- bin/world4/lib/count_in_array.rs
+++ bin/world4/lib/count_in_array.rs
@@ -8,14 +8,11 @@
 #![feature(map_first_last)]
 use std::collections::BTreeMap;
 use std::env;
-use std::fs::File;
+use std::fs;
 use std::io::BufReader;
 use std::io::BufWriter;
 use std::io::prelude::*;
 //use std::io::Result;
-
-
-const MAX_BLOCK_ENTRIES_COUNT:usize = 128*1024;
 
 struct Entry {
 	id: u64,
@@ -23,13 +20,15 @@ struct Entry {
 }
 
 struct BlockWriter {
-	map: BTreeMap<u64,u32>
+	map: BTreeMap<u64,u32>,
+	max_block_entries_count: usize,
 }
 
 impl BlockWriter {
-	fn new() -> Self {
+	fn new(max_count:usize) -> Self {
 		return BlockWriter {
-			map: BTreeMap::new()
+			map: BTreeMap::new(),
+			max_block_entries_count: max_count,
 		}
 	}
 
@@ -38,11 +37,11 @@ impl BlockWriter {
 	}
 
 	fn is_full(&self) -> bool {
-		return self.map.len() >= MAX_BLOCK_ENTRIES_COUNT;
+		return self.map.len() >= self.max_block_entries_count;
 	}
 
 	fn save(&mut self, filename:&String) {
-		let file = File::create(filename).unwrap();
+		let file = fs::File::create(filename).unwrap();
 		let mut buffered_file = BufWriter::with_capacity(65536, file);
 		for (id, count) in self.map.iter() {
 			buffered_file.write(&id.to_le_bytes());
@@ -53,12 +52,12 @@ impl BlockWriter {
 }
 
 struct BlockReader {
-	reader: BufReader<File>,
+	reader: BufReader<fs::File>,
 }
 
 impl BlockReader {
 	fn new(filename:&String) -> Self {
-		let file = File::open(filename).unwrap();
+		let file = fs::File::open(filename).unwrap();
 		return BlockReader {
 			reader: BufReader::with_capacity(65536, file),
 		};
@@ -108,7 +107,7 @@ impl BlockReaders {
 	}
 
 	fn add_reader_entry(&mut self, reader_id:&usize) -> bool {
-		let block_reader:&mut BlockReader = &mut self.readers[*reader_id];
+		let block_reader = &mut self.readers[*reader_id];
 		let result:Option<Entry> = block_reader.next();
 
 		if result.is_none() {
@@ -157,12 +156,17 @@ impl Iterator for BlockReaders {
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	let mut min_count:u32 = 0;
+	let mut max_block_entries:usize = 128*1024*1024;
 	if args.len() > 1 {
 		min_count = args[1].parse::<u32>().unwrap();
 	}
+	if args.len() > 2 {
+		max_block_entries = args[2].parse::<usize>().unwrap();
+	}
 	eprintln!("min_count: {}", min_count);
+	eprintln!("max_block_entries: {}", max_block_entries);
 
-	let mut block = BlockWriter::new();
+	let mut block = BlockWriter::new(max_block_entries);
 	let mut block_filenames:Vec<String> = Vec::new();
 
 	let stdin = std::io::stdin();
@@ -194,7 +198,7 @@ fn main() {
 							block.save(&filename);
 							block_filenames.push(filename);
 							drop(block);
-							block = BlockWriter::new();
+							block = BlockWriter::new(max_block_entries);
 						}
 					}
 					if code == 93 {
@@ -214,18 +218,29 @@ fn main() {
 			if count < &min_count {
 				continue;
 			}
+			//println!("{}\t{}", id, count);
 			println!("{}", id);
 		}
 	} else {
+		let filename:String = format!("count_in_array_{}.tmp", block_filenames.len());
+		block.save(&filename);
+		block_filenames.push(filename);
+
 		let mut block_readers = BlockReaders::new();
 		for filename in block_filenames.iter() {
 			block_readers.add_block_reader(filename);
 		}
+
 		for entry in block_readers {
 			if entry.count < min_count {
 				continue;
 			}
+			//println!("{}\t{}", entry.id, entry.count);
 			println!("{}", entry.id);
+		}
+
+		for filename in block_filenames.iter() {
+			fs::remove_file(filename);
 		}
 	}
 }
