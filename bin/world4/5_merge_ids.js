be9091825e3cd29e785e45f5dbdd4bcc97ba5ddc
Michael Kreil <github@michael-kreil.de>
2022-02-22 16:57:38 +0100

    Update 5_merge_ids.js

--- bin/world4/5_merge_ids.js
+++ bin/world4/5_merge_ids.js
@@ -10,15 +10,15 @@ const { uniqSortedLines, lineMerger, findDataFile, getDataFile, getTempFile, get
 start()
 
 function start() {
-	//let inputFilename1 = findDataFile('3_ids');
-	//let inputFilename2 = findDataFile('5_friend_ids');
-	//let tempFilename = getTempFile();
-	//let dataFilename = getDataFile('1_ids');
+	let inputFilename1 = findDataFile('3_ids');
+	let inputFilename2 = findDataFile('5_friend_ids');
+	let tempFilename = getTempFile();
+	let dataFilename = getDataFile('1_ids');
 
-	let inputFilename1 = 'lib/1_ids-2022_01_06_205348.tsv.xz';
-	let inputFilename2 = 'lib/5_friend_ids-2022_01_06_143013.tsv.xz';
-	let tempFilename   = 'lib/tempFile.tsv.xz';
-	let dataFilename   = 'lib/dataFile.tsv.xz';
+	//let inputFilename1 = 'lib/1_ids-2022_01_06_205348.tsv.xz';
+	//let inputFilename2 = 'lib/5_friend_ids-2022_01_06_143013.tsv.xz';
+	//let tempFilename   = 'lib/tempFile.tsv.xz';
+	//let dataFilename   = 'lib/dataFile.tsv.xz';
 
 	miss.pipe(
 		uniqSortedLines([
