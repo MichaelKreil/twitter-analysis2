652d87cec55b33f69002a113e30aa4a38e3bec82
Michael Kreil <github@michael-kreil.de>
2022-02-22 10:02:40 +0100

    Update 5_merge_ids.sh

--- bin/world4/5_merge_ids.sh
+++ bin/world4/5_merge_ids.sh
@@ -4,4 +4,4 @@ cd ${0%/*};
 
 rustc -C opt-level=3 -o lib/uniq lib/uniq.rs
 printf -v date '%(%Y-%m-%d-%H-%M-%S)T' -1
-( xz -dkc ~/data/twitter/world4/3_id-2021-12-28-22-26-15.tsv.xz; xz -dkc ~/data/twitter/world4/5_friend_ids-2022-01-06-14-30-13.tsv.xz ) | lib/uniq | xz -z9 > ~/data/twitter/world4/1_ids-${date}.tsv.xz
+( xz -dkc ~/data/twitter/world4/3_ids-2022_01_07_102301.tsv.xz; xz -dkc ~/data/twitter/world4/5_friend_ids-2022_02_21_140303.tsv.xz ) | lib/uniq | xz -z9 > ~/data/twitter/world4/1_ids-${date}.tsv.xz
