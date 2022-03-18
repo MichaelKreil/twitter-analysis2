7c44445e4cd2aed0219fd902c6bf2cf878f8072c
Michael Kreil <github@michael-kreil.de>
2022-03-19 00:44:14 +0100

    havel 0.1.5

--- bin/world4/convert.js
+++ bin/world4/convert.js
@@ -3,7 +3,9 @@
 const havel = require('havel');
 
 havel.pipeline(
-	havel.readFile('/root/data/twitter/world4/4_friends-2022_02_28_082704.tsv.xz', {showProgress:true}),
+	havel.readFile('/root/data/twitter/world4/4_friends-2022_02_28_082704.tsv.xz', {
+		showProgress: true
+	}),
 	havel.decompressXZ(),
 	havel.split(),
 	havel.map(line => {
@@ -11,7 +13,9 @@ havel.pipeline(
 		let key = line[0];
 		let value = JSON.parse(line[1]);
 		return new havel.KeyValue(key, value)
-	}, {outputType:'keyValue'} ),
+	}, {
+		outputType: 'keyValue'
+	}),
 	havel.eachPairWise((e1, e2) => {
 		if (e1.key.length < e2.key.length) return;
 		if ((e1.key.length > e2.key.length) || (e1.key >= e2.key)) {
