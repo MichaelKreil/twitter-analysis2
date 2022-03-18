8fea0249b3542e947964e3ba6c5aa96c71b732b9
Michael Kreil <github@michael-kreil.de>
2022-03-18 20:45:34 +0100

    fix converter

--- bin/world4/convert.js
+++ bin/world4/convert.js
@@ -6,8 +6,18 @@ havel.pipeline(
 	havel.readFile('/root/data/twitter/world4/4_friends-2022_02_28_082704.tsv.xz', {showProgress:true}),
 	havel.decompressXZ(),
 	havel.split(),
-	//havel.head(10),
-	havel.map(line => havel.KeyValue(...line.split('\t')), {outputType:'keyValue'} ),
+	havel.map(line => {
+		line = line.split('\t');
+		let key = line[0];
+		let value = JSON.parse(line[1]);
+		return new havel.KeyValue(key, value)
+	}, {outputType:'keyValue'} ),
+	havel.eachPairWise((e1, e2) => {
+		if (e1.key.length < e2.key.length) return;
+		if ((e1.key.length > e2.key.length) || (e1.key >= e2.key)) {
+			throw Error(`${JSON.stringify(e1.key)} >= ${JSON.stringify(e2.key)}`);
+		}
+	}),
 	havel.keyValueToStream(),
 	havel.writeFile('/root/data/twitter/world4/4_friends-2022_02_28_082704.bin'),
 	() => console.log('finished')
